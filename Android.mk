6084854d53a568cb6d2552db5585393e6820842d
jun.gao <jun.gao@amlogic.com>
2013-11-06 13:11:22 +0800

    initial fill project

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+#MAKE_JAR
+include $(CLEAR_VARS)
+LOCAL_MODULE_TAGS := optional 
+LOCAL_SRC_FILES := $(call all-subdir-java-files)
+LOCAL_MODULE := vmd
+include $(BUILD_JAVA_LIBRARY)
+#MAKE_XML
+include $(CLEAR_VARS)
+LOCAL_MODULE := com.amlogic.vmd.xml
+LOCAL_MODULE_TAGS := optional 
+#LOCAL_MODULE_CLASS := ETC
+LOCAL_MODULE_CLASS := JAVA_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
+LOCAL_SRC_FILES := $(LOCAL_MODULE)
+include $(BUILD_PREBUILT)
